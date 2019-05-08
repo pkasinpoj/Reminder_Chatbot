0fe933fe7057f711c624812dfbd9bc84838cb1d4
kasinpoj <colon007march@outlook.com>
2019-05-08 21:26:03 +0700

    update

--- src/main/java/com/chatbot/jumdee/LineBotController.java
+++ src/main/java/com/chatbot/jumdee/LineBotController.java
@@ -1,5 +1,4 @@
 package com.chatbot.jumdee;
-
 import classas.Payload;
 import com.google.gson.Gson;
 import com.linecorp.bot.client.LineSignatureValidator;
@@ -18,12 +17,7 @@ public class LineBotController
     @Qualifier("com.linecorp.channel_secret")
     String lChannelSecret;
 
-//    @Autowired
-//    @Qualifier("com.linecorp.channel_access_token")
-//    String lChannelAccessToken;
-
     @RequestMapping(value="/callback", method=RequestMethod.POST)
-
     public ResponseEntity<String> callback(
             @RequestHeader("X-Line-Signature") String aXLineSignature,
             @RequestBody String aPayload) throws IOException {
@@ -33,9 +27,7 @@ public class LineBotController
         final boolean valid=new LineSignatureValidator(lChannelSecret.getBytes()).validateSignature(aPayload.getBytes(), aXLineSignature);
         System.out.println("The signature is: " + (valid ? "valid" : "tidak valid"));
         if(aPayload!=null && aPayload.length() > 0)
-        {
-            System.out.println("Payload: " + aPayload);
-        }
+        { System.out.println("Payload: " + aPayload); }
         Gson gson = new Gson();
         Payload payload = gson.fromJson(aPayload, Payload.class);
         ManageEvent m = new ManageEvent();
