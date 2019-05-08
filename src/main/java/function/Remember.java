dad50ffff62c0e32cb771340c09f102ea9051c96
kasinpoj <colon007march@outlook.com>
2019-05-09 00:33:55 +0700

    update

--- src/main/java/function/Remember.java
+++ src/main/java/function/Remember.java
@@ -14,20 +14,21 @@ public class Remember {
         }
         return text;
     }
-    public String remembers(String msgText) throws IOException {
+    public String remembers(String msgText)  {
         String text ;
         String[] arrB = new String[2];
-        try {
             if (msgText.contains("ส่ง")){
                 arrB = msgText.split("ส่ง");
             } else if (msgText.contains("กำหนดส่ง")){
                 arrB = msgText.split("กำหนดส่ง");
             }
-            System.out.println("a"+arrB[0]);
-            text = "งานของคุณคือ"+"\n"+arrB[0]+"\n"+"กำหนดส่งคือ"+arrB[1]+"\n"+"บันทึกข้อมูลเสร็จเเล้วครับ";
-        }catch (Exception e){
-            text = "บันทึกไม่สำเร็จครับ";
-        }
+            if (arrB[0] != null && arrB[1] != null ){
+                text = "งานของคุณคือ"+"\n"+arrB[0]+"\n"+"กำหนดส่งคือ"+arrB[1]+"\n"+"บันทึกข้อมูลเสร็จเเล้วครับ";
+            }
+            else {
+                text = "บันทึกไม่สำเร็จครับ";
+            }
+
         return text;
     }
 
