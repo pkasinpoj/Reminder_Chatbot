284f0275feed34bfb5cebada39edfdd8663fb7fe
kasinpoj <colon007march@outlook.com>
2019-05-09 03:38:31 +0700

    update

--- src/main/java/function/Point.java
+++ src/main/java/function/Point.java
@@ -5,10 +5,24 @@ import java.io.IOException;
 public class Point {
     public String recievepoint(String msgText) throws IOException {
         String text;
+        String[] arrB = new String[3];
+        String[] cut = new String[3];
         try {
-            String[] arrB = msgText.split("-");
-            int point = Integer.parseInt(arrB[2]);
-            text = "ชื่อวิชาคือ" + "\n" + arrB[1] + "\n" + "คะเเนนของคุณคือ" + point + "\n" + "บันทึกข้อมูลเสร็จเเล้วครับ";
+            if (msgText.contains("จำคะเเนนเก็บ")){
+                arrB = msgText.split("จำคะเเนนเก็บ");
+            }else if (msgText.contains("จำคะเเนน")){
+                arrB = msgText.split("จำคะเเนน");
+            }
+            if (arrB[1].contains("=")){
+                cut = arrB[1].split("=");
+            }else if (arrB[1].contains("ได้")){
+                cut = arrB[1].split("ได้");
+            }
+            int point = Integer.parseInt(cut[1]);
+                text = "ชื่อวิชาคือ" + "\n" + cut[0] + "\n" + "คะเเนนของคุณคือ" + point + "\n" + "บันทึกข้อมูลเสร็จเเล้วครับ";
+//            String[] arrB = msgText.split("-");
+//            int point = Integer.parseInt(arrB[2]);
+//            text = "ชื่อวิชาคือ" + "\n" + arrB[1] + "\n" + "คะเเนนของคุณคือ" + point + "\n" + "บันทึกข้อมูลเสร็จเเล้วครับ";
         } catch (Exception e) {
             text = "ขออภัยด้วยครับน้องจำดีไม่สามารถบันทึกได้สำเร็จ";
         }
--- src/main/java/function/Remembersubject.java
+++ src/main/java/function/Remembersubject.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 public class Remembersubject {
     public String remembersubject(String msgText) throws IOException {
-        System.out.println("here");
         String text;
         String[] arrB = new String[3];
         String[] cut = new String[3];
@@ -14,15 +13,11 @@ public class Remembersubject {
             }else if (msgText.contains("วิชา")){
                 arrB = msgText.split("วิชา");
             }
-            System.out.println(arrB[1]);
             if (arrB[1].contains("เรียนวัน")){
                 cut = arrB[1].split("เรียนวัน");
             }else if (arrB[1].contains("เรียน")){
                 cut = arrB[1].split("เรียน");
             }
-//            System.out.println(cut[0]);
-//            System.out.println(cut[1]);
-////             System.out.println(cut[2]);
             if (cut[1].equals("จันทร์") || cut[1].equals("อังคาร") || cut[1].equals("พุธ") || cut[1].equals("พฤหัส") || cut[1].equals("ศุกร์") || cut[1].equals("เสาร์") || cut[1].equals("อาทิตย์")) {
                 text = "ชื่อวิชาคือ" + "\n" + cut[0] + "\n" + "เรียนวัน" + cut[1] + "\n" + "บันทึกข้อมูลเสร็จเเล้วครับ";
             } else {
