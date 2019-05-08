b0178aa4aeab27b489b64e07ac65c7f620f949a6
kasinpoj <colon007march@outlook.com>
2019-05-09 00:28:47 +0700

    update

--- src/main/java/function/Remember.java
+++ src/main/java/function/Remember.java
@@ -13,7 +13,22 @@ public class Remember {
             text = "บันทึกไม่สำเร็จครับ";
         }
         return text;
-        //////
+    }
+    public String remembers(String msgText) throws IOException {
+        String text ;
+        String[] arrB = new String[2];
+        try {
+            if (msgText.contains("ส่ง")){
+                arrB = msgText.split("ส่ง");
+            } else if (msgText.contains("กำหนดส่ง")){
+                arrB = msgText.split("กำหนดส่ง");
+            }
+            System.out.println("a"+arrB[0]);
+            text = "งานของคุณคือ"+"\n"+arrB[0]+"\n"+"กำหนดส่งคือ"+arrB[1]+"\n"+"บันทึกข้อมูลเสร็จเเล้วครับ";
+        }catch (Exception e){
+            text = "บันทึกไม่สำเร็จครับ";
+        }
+        return text;
     }
 
 }
--- src/main/java/gateway/Recieveinfo.java
+++ src/main/java/gateway/Recieveinfo.java
@@ -11,47 +11,33 @@ public class Recieveinfo {
     public String Recieveinfo (String msgText) throws IOException {
         String text;
         if (msgText.contains("ช่วยจำหน่อย") || msgText.contains("จำให้หน่อย") ){
-            try {
                 Greeting g = new Greeting();
                 String restext = g.greeting();
                 text = restext;
-            }catch (Exception e){
-                text = "อ่าาา ทำไมน้องจำดีไม่เข้าใจเลย";
-            }
         } else if (msgText.contains(",")){
-            try {
                 Remember r = new Remember();
                 String restext = r.remember(msgText);
                 text = restext;
-            }catch (Exception e){
-                text = "ไม่สามารถบันทึกได้ครับ";
-            }
         } else if (msgText.contains("จำวิชาเรียน") || msgText.contains("จำวิชา")  ){
-            try {
                 Remembersubject r = new Remembersubject();
                 String restext = r.remembersubject(msgText);
                 text = restext;
-            }catch (Exception e){
-                text = "มันคืออะไรหรอครับ";
-            }
         } else if (msgText.contains("จำคะเเนนเก็บ") || msgText.contains("จำคะเเนน")  ){
-            try {
                 Point p = new Point();
                 String restext = p.recievepoint(msgText);
                 text = restext;
-            }catch (Exception e){
-                text = "มันคืออะไรหรอครับ";
-            }
         } else if (msgText.contains("ดูคะเเนนเก็บ") || msgText.contains("ดูคะเเนน")  ){
-            try {
                 Point p = new Point();
                 String restext = p.getpoint(msgText);
                 text = restext;
-            }catch (Exception e){
-                text = "มันคืออะไรหรอครับ";
-            }
         } else  {
-            text = "มันคืออะไรอ่าาาาครับ";
+            try {
+                Remember r = new Remember();
+                String restext = r.remembers(msgText);
+                text = restext;
+            }catch (Exception e){
+                text = "มันคืออะไรอ่าาาาครับ";
+            }
         }
         return text;
     }
