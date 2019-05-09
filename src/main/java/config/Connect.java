d17fa974f47c6d9eab717f83539f0b9027276580
kasinpoj <colon007march@outlook.com>
2019-05-10 05:39:39 +0700

    update

--- src/main/java/config/Connect.java
+++ src/main/java/config/Connect.java
@@ -6,7 +6,7 @@ public class Connect {
     public static Connection connectiondb(){
         try{
             System.out.println("here");
-           Class.forName("mysql.jdbc.Driver");
+           Class.forName("com.mysql.jdbc.Driver");
            String url = "jdbc:mysql://localhost/jumdee";
            Connection con = DriverManager.getConnection(url,"root","");
             System.out.println("Succss");
