21ebd76b71d4671b26ed32798b6668aa9d55a4c6
kasinpoj <colon007march@outlook.com>
2019-05-10 23:04:12 +0700

    update

--- src/main/java/config/Connect.java
+++ src/main/java/config/Connect.java
@@ -7,8 +7,8 @@ public class Connect {
         try{
             System.out.println("here");
                 Class.forName("com.mysql.cj.jdbc.Driver");//ระบุ Driver
-                String url = "jdbc:mysql://localhost:3306/jumdeechatbot";
-                Connection connect = DriverManager.getConnection(url, "root","");
+                String url = "jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/heroku_50236681737c507";
+                Connection connect = DriverManager.getConnection(url, "bddf2105a1f693","4e10673e");
                 System.out.println("เชื่อมต่อฐานข้อมูลเรียบร้อย");
                 return connect;
         }catch (Exception e){
