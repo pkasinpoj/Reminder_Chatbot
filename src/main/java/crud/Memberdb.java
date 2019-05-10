85aba03e6ac9e75e80f31426e5d643dcdf599f19
kasinpoj <colon007march@outlook.com>
2019-05-11 03:28:12 +0700

    update

--- src/main/java/crud/Memberdb.java
+++ src/main/java/crud/Memberdb.java
@@ -40,6 +40,8 @@ public class Memberdb {
                 arr = em.split("\n");
             }
             System.out.println(arr[0]);
+            System.out.println(arr[1]);
+            System.out.println(arr[2]);
             rs.close();
             con.close();
             return result;
